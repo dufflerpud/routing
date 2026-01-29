2baaa4e713fd06aed6e2ddc05537ed1955c71477
Christopher Caldwell <dufflerpud@yahoo.com>
2026-01-29 17:52:05 -0500

    Use cpi tools, allow sign:user notation

--- src/distuser_to_pdf.pl
+++ src/distuser_to_pdf.pl
@@ -12,234 +12,47 @@
 
 use strict;
 
+use lib "/usr/local/lib/perl";
+
+use cpi_file qw( fatal echodo mkdirp files_in tempfile cleanup );
+use cpi_send_file qw( sendmail );
+use cpi_arguments qw( parse_arguments );
+use cpi_perl qw( quotes );
+use cpi_filename qw( text_to_filename );
+use cpi_vars;
+
 # Put constants here
 
 my $PROJECT = "routing";
 my $PROG = ( $_ = $0, s+.*/++, s/\.[^\.]*$//, $_ );
-my $TMP = "/tmp/$PROG.$$";
-#my $TMP = "/tmp/$PROG";
 my $TRIP_DIR="/var/log/$PROJECT/trips";
 my $BASEDIR = "/usr/local/projects/$PROJECT";
 my $SPLIT_TRIPS = "$BASEDIR/bin/split_trips";
 my $READ_TRIPS = "$BASEDIR/bin/read_trips";
+my $SIGN = "/var/www/www/sto/sign/index.cgi";
 
 my $DEFAULT_DISTRIBUTOR = "MOW_Sagadahoc";
 my $DEFAULT_DRIVER = "Chris_Caldwell";
 
-my %ONLY_ONE_DEFAULTS =
+our %ONLY_ONE_DEFAULTS =
     (
     #"i"	=>	"/var/log/routing/trips/$DEFAULT_DISTRIBUTOR/$DEFAULT_DRIVER/log",
-    "i"	=>	"",
-    "t"	=>	$TMP,
-    "d"	=>	$DEFAULT_DISTRIBUTOR,
-    "r" =>	"",
-    "u"	=>	$DEFAULT_DRIVER,
-    "o"	=>	"/tmp/fun.pdf",
-    "m"	=>	"",
-    "v"	=>	""
+    "input_file"	=>	"",
+    "tempdir"		=>	&tempfile(".dir"),
+    "distributor"	=>	$DEFAULT_DISTRIBUTOR,
+    "rate" 		=>	"",
+    "user"		=>	$DEFAULT_DRIVER,
+    "output"		=>	"/tmp/fun.pdf",
+    "month"		=>	"",
+    "verbosity"		=>	0
     );
 
 # Put variables here.
 
-my @problems;
-my %ARGS;
-my @files;
-my $exit_stat = 0;
-
-# Put interesting subroutines here
-
-#=======================================================================#
-#	Verbatim from prototype.pl					#
-#=======================================================================#
-
-#########################################################################
-#	Print a header if need be.					#
-#########################################################################
-my $hdrcount = 0;
-sub CGIheader
-    {
-    print "Content-type:  text/html\n\n" if( $hdrcount++ == 0 );
-    }
-
-#########################################################################
-#	Print out a list of error messages and then exit.		#
-#########################################################################
-sub fatal
-    {
-    if( ! $ENV{SCRIPT_NAME} )
-        { print join("\n",@_,""); }
-    else
-        {
-	&CGIheader();
-	print "<h2>Fatal error:</h2>\n",
-	    map { "<dd><font color=red>$_</font>\n" } @_;
-	}
-    exit(1);
-    }
-
-
-#########################################################################
-#	Put <form> information into %FORM (from STDIN or ENV).		#
-#########################################################################
-my %FORM;
-sub CGIreceive
-    {
-    my ( $name, $value );
-    my ( @fields, @ignorefields, @requirefields );
-    my ( @parts );
-    my $incoming = "";
-    return if ! defined( $ENV{'REQUEST_METHOD'} );
-    if ($ENV{'REQUEST_METHOD'} eq "POST")
-	{ read(STDIN, $incoming, $ENV{'CONTENT_LENGTH'}); }
-    else
-	{ $incoming = $ENV{'QUERY_STRING'}; }
-    
-    if( defined($ENV{"CONTENT_TYPE"}) &&
-        $ENV{"CONTENT_TYPE"} =~ m#^multipart/form-data# )
-	{
-	my $bnd = $ENV{"CONTENT_TYPE"};
-	$bnd =~ s/.*boundary=//;
-	foreach $_ ( split(/--$bnd/s,$incoming) )
-	    {
-	    if( /^[\r\n]*[^\r\n]* name="([^"]*)"[^\r\n]*\r*\nContent-[^\r\n]*\r*\n\r*\n(.*)[\r]\n/s )
-		{
-		#### Skip generally blank fields
-		next if ($2 eq "");
-
-		#### Allow for multiple values of a single name
-		$FORM{$1} .= "," if ($FORM{$1} ne "");
-
-		$FORM{$1} .= $2;
-
-		#### Add to ordered list if not on list already
-		push (@fields, $1) unless (grep(/^$1$/, @fields));
-		}
-	    elsif( /^[\r\n]*[^\r\n]* name="([^"]*)"[^\r\n]*\r*\n\r*\n(.*)[\r]\n/s )
-		{
-		#### Skip generally blank fields
-		next if ($2 eq "");
-
-		#### Allow for multiple values of a single name
-		$FORM{$1} .= "," if (defined($FORM{$1}) && $FORM{$1} ne "");
-
-		$FORM{$1} .= $2;
-
-		#### Add to ordered list if not on list already
-		push (@fields, $1) unless (grep(/^$1$/, @fields));
-		}
-	    }
-	}
-    else
-	{
-	foreach ( split(/&/, $incoming) )
-	    {
-	    ($name, $value) = split(/=/, $_);
-
-	    $name  =~ tr/+/ /;
-	    $value =~ tr/+/ /;
-	    $name  =~ s/%([A-F0-9][A-F0-9])/pack("C", hex($1))/gie;
-	    $value =~ s/%([A-F0-9][A-F0-9])/pack("C", hex($1))/gie;
-
-	    #### Strip out semicolons unless for special character
-	    $value =~ s/;/$$/g;
-	    $value =~ s/&(\S{1,6})$$/&$1;/g;
-	    $value =~ s/$$/ /g;
-
-	    #$value =~ s/\|/ /g;
-	    $value =~ s/^!/ /g; ## Allow exclamation points in sentences
-
-	    #### Split apart any directive prefixes
-	    #### NOTE: colons are reserved to delimit these prefixes
-	    @parts = split(/:/, $name);
-	    $name = $parts[$#parts];
-	    if (grep(/^require$/, @parts))
-		{
-		push (@requirefields, $name);
-		}
-	    if (grep(/^ignore$/, @parts))
-		{
-		push (@ignorefields, $name);
-		}
-	    if (grep(/^dynamic$/, @parts))
-		{
-		#### For simulating a checkbox
-		#### It may be dynamic, but useless if nothing entered
-		next if ($value eq "");
-		$name = $value;
-		$value = "on";
-		}
-
-	    #### Skip generally blank fields
-	    next if ($value eq "");
-
-	    #### Allow for multiple values of a single name
-	    $FORM{$name} .= "," if( defined($FORM{$name}) && $FORM{$name} ne "");
-	    $FORM{$name} .= $value;
-
-	    #### Add to ordered list if not on list already
-	    push (@fields, $name) unless (grep(/^$name$/, @fields));
-	    }
-	}
-    }
-
-#########################################################################
-#	Print a command and then execute it.				#
-#########################################################################
-sub echodo
-    {
-    my( $cmd ) = @_;
-    if( ! $ARGS{v} )
-	{ }	# No need to print commands
-    elsif( $ENV{SCRIPT_NAME} )
-	{ print "<pre>+ $cmd</pre>\n"; }
-    else
-        { print "+ $cmd\n"; }
-    return system( $cmd );
-    }
-
-#########################################################################
-#	Read an entire file and return the contents.			#
-#	If open fails and a return value is not specified, fail.	#
-#########################################################################
-sub read_file
-    {
-    my( $fname, $ret ) = @_;
-    if( open(COM_INF,$fname) )
-        {
-	$ret = do { local $/; <COM_INF> };
-	close( COM_INF );
-	}
-    elsif( scalar(@_) < 2 )
-        { &fatal("Cannot open ${fname}:  $!"); }
-    return $ret;
-    }
-
-#########################################################################
-#	Read an entire file.						#
-#########################################################################
-sub write_file
-    {
-    my( $fname, @contents ) = @_;
-    open( COM_OUT, "> $fname" ) || &fatal("Cannot write ${fname}:  $!");
-    print COM_OUT @contents;
-    close( COM_OUT );
-    }
-
-#=======================================================================#
-#	New code not from prototype.pl					#
-#		Should at least include:				#
-#			parse_arguments()				#
-#			CGI_arguments()					#
-#			usage()						#
-#=======================================================================#
-
-#########################################################################
-#	Setup arguments if CGI.						#
-#########################################################################
-sub CGI_arguments
-    {
-    &CGIreceive();
-    }
+our @problems;
+our %ARGS;
+our @files;
+our $exit_stat = 0;
 
 #########################################################################
 #	Print usage message and die.					#
@@ -249,64 +62,13 @@ sub usage
     &fatal( @_, "",
 	"Usage:  $PROG <possible arguments>","",
 	"where <possible arguments> is:",
-	"    -o <output file>",
-	"    -t <title>",
-	"    <filename>",
-	"    <filename> -t <filename title>"
+	"    -output <output file or e-mail address>",
+	"    -rate x.xx",
+	"    -user <driver>",
+	"    -distributor <distributor>",
+	"    -month yyyy-mm",
+	"    -verbosity 0 or 1"
 	);
-    }
-
-#########################################################################
-#	Parse the arguments						#
-#########################################################################
-sub parse_arguments
-    {
-    my $arg;
-    while( defined($arg = shift(@ARGV) ) )
-	{
-	# Put better argument parsing here.
-
-	if( $arg =~ /^-(.)(.*)$/ && defined($ONLY_ONE_DEFAULTS{$1}) )
-	    {
-	    if( defined($ARGS{$1}) )
-		{ push( @problems, "-$1 specified multiple times." ); }
-	    else
-		{ $ARGS{$1} = ( $2 ne "" ? $2 : shift(@ARGV) ); }
-	    }
-	elsif( $arg =~ /^-(t)(.*)$/ )
-	    {
-	    my $val = ( $2 ? $2 : shift(@ARGV) );
-	    if( $#files <= 0 )
-	        {
-		if( defined($files[$#files]->{$1}) )
-		    {
-		    push( @problems,
-			$files[$#files]->{name} .
-			    " -$1 specified multiple times." );
-		    }
-		else
-		    { $files[$#files]->{$1} = $val; }
-		}
-	    elsif( defined( $ARGS{$1} ) )
-		{ push( @problems, "-$1 specified multiple times." ); }
-	    else
-		{ $ARGS{$1} = $val; }
-	    }
-	elsif( $arg =~ /^-.*/ )
-	    { push( @problems, "Unknown argument [$arg]" ); }
-	else
-	    { push( @files, $arg ); }
-	}
-
-    #push( @problems, "No files specified" ) if( ! @files );
-    &usage( @problems ) if( @problems );
-
-    # Put interesting code here.
-
-    grep( $ARGS{$_}=(defined($ARGS{$_})?$ARGS{$_}:$ONLY_ONE_DEFAULTS{$_}),
-	keys %ONLY_ONE_DEFAULTS );
-
-    $ARGS{i} ||= "$TRIP_DIR/$ARGS{d}/$ARGS{u}.log";
     }
 
 #########################################################################
@@ -315,10 +77,10 @@ sub parse_arguments
 #########################################################################
 sub do_it
     {
-    my $trips_dir = "$ARGS{t}/$ARGS{d}/$ARGS{u}";
+    my $trips_dir = "$ARGS{tempdir}/$ARGS{distributor}/$ARGS{user}";
     my %fnames =
 	(
-	log	=> $ARGS{i}||$trips_dir."/log",
+	log	=> $ARGS{input_file}||$trips_dir."/log",
 	data	=> $trips_dir,
 	trips	=> $trips_dir."/trips",
 	htmls	=> $trips_dir."/htmls",
@@ -326,67 +88,75 @@ sub do_it
 	);
 
     chdir( $BASEDIR ) || &fatal("Cannot chdir($BASEDIR):  $!");
-    grep( -d $fnames{$_} || &echodo("mkdir -p $fnames{$_}"), "data", "htmls", "pdfs" );
+    &mkdirp( 0755, $fnames{data}, $fnames{htmls}, $fnames{pdfs} );
 
-    &echodo( join(" ",
+    &echodo(
 	    $SPLIT_TRIPS,
-	    "-i", $fnames{log},
-	    "-o", $fnames{data}
-	    ) );
+	    "-v",	$ARGS{verbosity},
+	    "-i",	$fnames{log},
+	    "-o",	$fnames{data}
+	    );
 
     my @pdfs = ();
-    opendir( D, $fnames{trips} ) || &fatal("Cannot opendir($fnames{trips}):  $!");
-
-    my $l4 = $ARGS{m};
+    my $l4 = $ARGS{month};
     $l4 = "$1-$2" if( $l4 && $l4 =~ /(\d\d\d\d)(\d\d)/ );
-    foreach my $trips_file ( grep( /\d\d\d\d-\d\d-\d\d/, readdir(D) ) )
+    #foreach my $trips_file ( grep( /\d\d\d\d-\d\d-\d\d/, readdir(D) ) )
+    print STDERR "Looking for [$l4] in [$fnames{trips}]\n";
+    foreach my $trips_file ( &files_in($fnames{trips},"\\d\\d\\d\\d-\\d\\d-\\d\\d.*") )
 	{
-	#print STDERR "l4=$l4 trips_file=$trips_file.\n";
+	print STDERR "l4=$l4 trips_file=$trips_file.\n";
 	next if( $l4 && $trips_file !~ /^$l4/ );
-	#print STDERR "Processing $trips_file.\n" if( 1 || $ARGS{v} );
-	&echodo( join(" ",
+	print STDERR "Processing $fnames{trips}/$trips_file.\n" if( 1 || $ARGS{verbosity} );
+	&echodo(
 	    $READ_TRIPS,
-	    ( $ARGS{r} ? ("-r",	$ARGS{r}) : () ),
-	    "-d",	$ARGS{d},
-	    "-u",	$ARGS{u},
+	    ( $ARGS{rate} ? ("-r", $ARGS{rate}) : () ),
+	    "-d",	$ARGS{distributor},
+	    "-u",	$ARGS{user},
 	    "-i",	"$fnames{trips}/$trips_file",
 	    "-h",	"$fnames{htmls}/$trips_file.html",
 	    "-o",	"$fnames{pdfs}/$trips_file.pdf"
-	    ) );
+	    );
 	push( @pdfs, "$fnames{pdfs}/$trips_file.pdf" );
+	&echodo("ls -ld '$fnames{pdfs}/$trips_file.pdf'");
 	}
-    closedir( D );
 
     if( ! @pdfs )
-        { &fatal("No expenses/trips to report for $ARGS{d}."); }
-    elsif( $ARGS{o} !~ /\@/ )
-        { &echodo( join(" ","pdfunite",@pdfs,$ARGS{o}) ); }
-    else
+        { &fatal("No expenses/trips to report for $ARGS{distributor}."); }
+    elsif( $ARGS{output} =~ /\@/ )
 	{
-	my $outfile = "$TMP.pdf";
-        &echodo( join(" ","pdfunite",@pdfs,$outfile) );
-	my $msg = "$ARGS{d} log of $ARGS{u}";
-	$msg .= sprintf(" of %s",$ARGS{m}) if( $ARGS{m} );
+	my $outfile = &tempfile(".pdf");
+        &echodo( "pdfunite",@pdfs,$outfile );
+	my $msg = "$ARGS{distributor} log of $ARGS{user}";
+	$msg .= sprintf(" of %s",$ARGS{month}) if( $ARGS{month} );
 	$msg =~ s/_+/ /g;
-	open( OUT, "| mailx -s '$msg' -a $outfile $ARGS{o}" )
-	    || &fatal("Cannot e-mail to $ARGS{o}:  $!");
-	print OUT "$msg in enclosed PDF file.\n";
-	close( OUT );
-	unlink( $outfile );
+	&sendmail( "$PROJECT\@$cpi_vars::DOMAIN",$ARGS{output},$msg,
+	    "$msg in enclosed PDF file.\n", $outfile );
 	}
+    elsif( $ARGS{output} =~ /^sign:(.*)/ )
+        {
+	my $sign_user = $1;
+	&mkdirp( 0755, $ARGS{tempdir} );
+	my $filepiece = "$ARGS{distributor}-$ARGS{user}";
+	$filepiece .= "-$ARGS{month}" if( $ARGS{month} );
+	$filepiece .= "-log.pdf";
+	my $outfile = "$ARGS{tempdir}/".&text_to_filename($filepiece);
+        &echodo( "pdfunite",@pdfs,$outfile );
+	&echodo( $SIGN, "handoff", &quotes($sign_user,$outfile) );
+	}
+    else
+        { &echodo( join(" ","pdfunite",@pdfs,$ARGS{output}) ); }
     }
 
 #########################################################################
 #	Main								#
 #########################################################################
 
-if( 0 && $ENV{SCRIPT_NAME} )
-    { &CGI_arguments(); }
-else
-    { &parse_arguments(); }
+&parse_arguments();
+$ARGS{input_file} ||= "$TRIP_DIR/$ARGS{distributor}/$ARGS{user}.log";
+$cpi_vars::VERBOSITY = $ARGS{verbosity};
 
 #print join("\n\t","Args:",map{"$_:\t$ARGS{$_}"} sort keys %ARGS), "\n";
 
 &do_it();
 
-exec("rm -rf $TMP");
+&cleanup(0);
